2a20958567b5c7d89e9d9af2ca4f33a580446f99
Patrick McManis <pytho834@gmail.com>
2015-02-01 15:29:34 -0800

    added star image

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,14 @@ include Faker
 
 Recipe.destroy_all
 
-ingredients = ["mushrooms", "olives", "cheese"]
+ingredients = ['"mushrooms", "olives", "cheese"']
 
 5.times do
   recipe = Recipe.create(user_id: rand(0..100),
                          title: Commerce.product_name,
                          serving_size: Commerce.price.round(0),
-                         directions: Lorem.sentences(sentence_count=3).join(","),
-                         picture: Avatar.image(nil, "50x50"),
+                         directions: Lorem.sentences(sentence_count=3).join(','),
+                         picture: Avatar.image(nil, '50x50'),
                          ingredients: ingredients[rand(0..2)],
                          category: Company.suffix,
                          cook_time: Commerce.price.round(0),
@@ -39,14 +39,15 @@ Product.destroy_all
 
 5.times do
   product = Product.create(name: Commerce.product_name.titleize,
-                           description: Lorem.paragraphs(5).join(" "),
+                           description: Lorem.paragraphs(5).join(' '),
                            quantity: rand(1..100),
                            price: Commerce.price,
                            cost: Commerce.price,
                            weight: rand(1.5..3),
-                           thumbnail: Avatar.image(nil, "50x50"),
-                           image: Avatar.image(nil, "150x150"),
+                           thumbnail: Avatar.image(nil, '50x50'),
+                           image: Avatar.image(nil, '150x150'),
 
   )
   puts product.inspect
+
 end
