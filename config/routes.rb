98c330ffe1fabd6e29ccaede33051bc4ab76df71
Radley Anaya <radmation@yahoo.com>
2015-02-02 01:24:00 -0800

    Fixed routes
    Gotta add the broken routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,17 @@
 Rails.application.routes.draw do
 
 
-<<<<<<< HEAD
-
-
-=======
-  resources :recipe_comments
->>>>>>> origin/master
-
   resources :recipes do
     resources :recipe_comments
     get 'page/:page', :action => :index, :on => :collection
-<<<<<<< HEAD
+
   end
 
 
   resources :forums do
     resources :comments
   end
-=======
 
->>>>>>> origin/master
 
     resources :forums do
       resources :comments
@@ -93,5 +84,4 @@ Rails.application.routes.draw do
     #     # (app/controllers/admin/products_controller.rb)
     #     resources :products
     #   end
-  end
 end
