911bf174343207760cbee88664d2b8f3f47130f3
Pheng <phengxiong509.com>
2015-02-01 21:42:48 -0800

    adjust schema
    adjust schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150201195802) do
+ActiveRecord::Schema.define(version: 20150202002927) do
+
+  create_table "comments", force: true do |t|
+    t.string   "author"
+    t.text     "content"
+    t.datetime "date_published"
+    t.integer  "forum_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["forum_id"], name: "index_comments_on_forum_id"
+
+  create_table "forums", force: true do |t|
+    t.integer  "forum_id"
+    t.string   "author"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "published_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "name"
-    t.text     "description"
+    t.string   "description"
     t.integer  "quantity"
     t.float    "price"
     t.float    "cost"
@@ -24,6 +45,16 @@ ActiveRecord::Schema.define(version: 20150201195802) do
     t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "recipe_comments", force: true do |t|
+    t.integer  "recipe_id"
+    t.string   "author"
+    t.text     "opinion"
+    t.integer  "rating"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "recipes", force: true do |t|
