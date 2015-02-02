0862b71679f072672987175933e742963739bd4d
Pheng <phengxiong509.com>
2015-02-02 08:27:28 -0800

    adjust routes
    adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,34 @@
 Rails.application.routes.draw do
 
-	resources :products do
-		resources :reviews
-		get 'page/:page', :action => :index, :on => :collection
-	end
 
-  resources :products, :path => 'store/products'
+  resources :recipes do
+    resources :recipe_comments
+    get 'page/:page', :action => :index, :on => :collection
 
-  resources :recipes
+  end
+
+  resources :products do
+    resources :reviews
+    get 'page/:page', :action => :index, :on => :collection
+  end
+
+  resources :forums do
+    resources :comments
+  end
+
+
+  resources :forums do
+    resources :comments
+
+  end
+
+  resources :products, :path => "store/products"
+
 
   root 'page#Home'
 
   get '/recipes', to: 'recipes#index'
-  get '/forum', to: 'page#forum', as: 'forum'
+  get '/forum', to: 'forums#index'
 
 
   resources :logins
@@ -73,163 +89,3 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
-
-  resources :recipe_comments
-
-
-  resources :recipes do
-    resources :recipe_comments
-    get 'page/:page', :action => :index, :on => :collection
-
-  end
-
-
-  resources :forums do
-    resources :comments
-  end
-
-    resources :forums do
-      resources :comments
-
-    end
-
-    resources :products, :path => "store/products"
-
-
-    root 'page#Home'
-
-    get '/recipes', to: 'recipes#index'
-    get '/forum', to: 'forums#index'
-
-
-    resources :logins
-
-    devise_for :users
-
-    # The priority is based upon order of creation: first created -> highest priority.
-    # See how all your routes lay out with "rake routes".
-
-    # You can have the root of your site routed with "root"
-
-    # Example of regular route:
-    #   get 'products/:id' => 'catalog#view'
-
-    # Example of named route that can be invoked with purchase_url(id: product.id)
-    #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-    # Example resource route (maps HTTP verbs to controller actions automatically):
-    #   resources :products
-
-    # Example resource route with options:
-    #   resources :products do
-    #     member do
-    #       get 'short'
-    #       post 'toggle'
-    #     end
-    #
-    #     collection do
-    #       get 'sold'
-    #     end
-    #   end
-
-    # Example resource route with sub-resources:
-    #   resources :products do
-    #     resources :comments, :sales
-    #     resource :seller
-    #   end
-
-    # Example resource route with more complex sub-resources:
-    #   resources :products do
-    #     resources :comments
-    #     resources :sales do
-    #       get 'recent', on: :collection
-    #     end
-    #   end
-
-    # Example resource route with concerns:
-    #   concern :toggleable do
-    #     post 'toggle'
-    #   end
-    #   resources :posts, concerns: :toggleable
-    #   resources :photos, concerns: :toggleable
-
-    # Example resource route within a namespace:
-    #   namespace :admin do
-    #     # Directs /admin/products/* to Admin::ProductsController
-    #     # (app/controllers/admin/products_controller.rb)
-    #     resources :products
-    #   end
-
-
-	resources :products do
-		resources :reviews
-		get 'page/:page', :action => :index, :on => :collection
-	end
-
-  resources :products, :path => 'store/products'
-
-  resources :recipes
-
-  root 'page#Home'
-
-  get '/recipes', to: 'recipes#index'
-  get '/forum', to: 'page#forum', as: 'forum'
-
-
-  resources :logins
-
-  devise_for :users
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
