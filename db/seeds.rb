d61f082c384c33c23eefc5f1fb0680d83b542f34
Pheng <phengxiong509.com>
2015-02-01 21:56:51 -0800

    adjust
    adjust

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,18 +10,8 @@ include Faker
 
 Recipe.destroy_all
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 ingredients = ["Mushrooms", "Olives", "Cheese", "Bacon"]
-=======
-ingredients = ['"mushrooms", "olives", "cheese"']
->>>>>>> origin/Patricks_branch
-=======
-ingredients = ['"mushrooms", "olives", "cheese"']
-=======
-ingredients = ["Mushrooms", "Olives", "Cheese", "Bacon"]
->>>>>>> origin/master
->>>>>>> e002f4d55d9c8812d191a7de21e6a4968f4f675a
+
 
 rand_food = ["100", "150", "200", "250", "300","350", "400", "450", "500", "550", "600", "650", "700", "750", "800", "850", "900"]
 
@@ -30,22 +20,18 @@ rand_food = ["100", "150", "200", "250", "300","350", "400", "450", "500", "550"
   recipe = Recipe.create( user_id: User.offset(rand(User.count)).first.id, #gets an actual user_id
                          title: Commerce.product_name,
                          serving_size: Commerce.price.round(0),
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                          directions: Lorem.sentences(sentence_count=3).join(","),
                          picture: "http://baconmockup.com/"+ rand_food[rand(2..16)]  +"/"+ rand_food[rand(2..16)] + "",
-=======
+
+                         directions: Lorem.sentences(sentence_count=3).join(','),
+
                          directions: Lorem.sentences(sentence_count=3).join(','),
                          picture: Avatar.image(nil, '50x50'),
->>>>>>> origin/Patricks_branch
-=======
-                         directions: Lorem.sentences(sentence_count=3).join(','),
-                         picture: Avatar.image(nil, '50x50'),
-=======
+
                          directions: Lorem.sentences(sentence_count=3).join(","),
                          picture: "http://baconmockup.com/"+ rand_food[rand(2..16)]  +"/"+ rand_food[rand(2..16)] + "",
->>>>>>> origin/master
->>>>>>> e002f4d55d9c8812d191a7de21e6a4968f4f675a
+
                          ingredients: ingredients[rand(0..2)],
                          category: Company.suffix,
                          cook_time: Commerce.price.round(0),
@@ -81,11 +67,6 @@ Product.destroy_all
   )
   puts product.inspect
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> e002f4d55d9c8812d191a7de21e6a4968f4f675a
 
 end
 5.times do
@@ -108,10 +89,5 @@ end
   puts forum.inspect
 
 
-<<<<<<< HEAD
-=======
->>>>>>> origin/Patricks_branch
-=======
->>>>>>> origin/master
->>>>>>> e002f4d55d9c8812d191a7de21e6a4968f4f675a
+
 end
