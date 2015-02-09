4261757098408a0ad2268094ae6eec1700ad1ded
Radley Anaya <radmation@yahoo.com>
2015-02-08 21:06:42 -0800

    Added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202002927) do
+ActiveRecord::Schema.define(version: 20150209014008) do
 
   create_table "comments", force: true do |t|
     t.string   "author"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20150202002927) do
     t.string   "prepare_time"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "shared"
   end
 
   create_table "reviews", force: true do |t|
