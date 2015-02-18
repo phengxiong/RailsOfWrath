17f5d675dd4a7f5762d9cc479308bf084399cdad
Pheng <phengxiong509.com>
2015-02-17 22:41:10 -0800

    first commit iteration 5
    added some css
    modifications to products show page
    modifications to recipes show page
    added category field to products
    added description field to recipes
    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Forum.destroy_all
+
 include Faker
 
 Recipe.destroy_all
 
-ingredients = ["Mushrooms", "Olives", "Cheese", "Bacon"]
+ingredients = ["Mushrooms",
+               "Olives",
+               "Cheese",
+               "Bacon",
+               "Pepperoni",
+               "Chicken",
+               "Beef",
+               "Green Peppers"]
+
+
+recipe_category = ["Appetizer",
+                   "Breakfast & Lunch",
+                   "Chicken",
+                   "Dessert",
+                   "Healthy",
+                   "Main Dish",
+                   "Slow Cooker",
+                   "Vegetarian"]
 
 50.times do
 
@@ -19,10 +36,11 @@ ingredients = ["Mushrooms", "Olives", "Cheese", "Bacon"]
                          serving_size: Commerce.price.round(0),
                          directions: Lorem.paragraphs(5).join(" "),
                          picture: "http://lorempixel.com/400/200/food",
-                         ingredients: ingredients[rand(0..2)],
-                         category: Company.suffix,
+                         ingredients: ingredients.sort_by { rand }[0..rand(ingredients.length)].join(", "),
+                         category: recipe_category[rand(recipe_category.length)],
                          cook_time: Commerce.price.round(0),
                          prepare_time: Commerce.price.round(0),
+                         description: Lorem.paragraphs(2).join(" ")
 
   )
   puts recipe.inspect
@@ -39,6 +57,14 @@ ingredients = ["Mushrooms", "Olives", "Cheese", "Bacon"]
 end
 
 Product.destroy_all
+product_category = ["Cookware",
+                    "Bakeware",
+                    "Kitchen Tools",
+                    "Electrics",
+                    "Cutlery",
+                    "BBQ & Outdoor",
+                    "Tabletop & Serving",
+                    "Storage & Cleaning"]
 
 20.times do
   product = Product.create(name: Commerce.product_name.titleize,
@@ -49,13 +75,13 @@ Product.destroy_all
                            weight: rand(1.5..3),
                            thumbnail: Avatar.image(nil, '50x50'),
                            image: Avatar.image(nil, '150x150'),
+                           category: product_category[rand(product_category.length)]
 
   )
   puts product.inspect
-
-
 end
 
+Forum.destroy_all
 5.times do
   forum = Forum.create(title: Name.title,
                        author: Name.name,
