2ed2f1d8caa7538177ab4ca2f3e4536ee91fd7ab
Radley Anaya <radmation@yahoo.com>
2015-02-23 08:20:45 -0800

    deleted migration not needed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223024756) do
+ActiveRecord::Schema.define(version: 20150222163344) do
 
   create_table "comments", force: true do |t|
     t.string   "author"
@@ -79,8 +79,8 @@ ActiveRecord::Schema.define(version: 20150223024756) do
     t.string   "prepare_time"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "author"
     t.integer  "shared"
+    t.string   "author"
     t.text     "description"
   end
 
